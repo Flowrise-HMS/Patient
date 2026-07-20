aeae3a15a06dd96f738acefb2058c6085ca83d9f
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-07-20 01:47:07 +0000

    fix: api routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,5 @@ use Modules\Core\Services\ApiRouteRegistrar;
 use Modules\Patient\Http\Controllers\Api\PatientController;
 
 ApiRouteRegistrar::register(
-    namePrefix: 'patients',
     routes: fn () => Route::apiResource('patients', PatientController::class)->only(['index', 'show']),
 );
