7758c6e7fa2a23aac542e39de342e0779fee97c2
musheabdulhakim <musheabdulhakim99@gmail.com>
2026-09-19 19:36:51 +0000

    feat: place the patients cluster in the Patient Care sidebar group

--- app/Filament/Clusters/Patient/PatientCluster.php
+++ app/Filament/Clusters/Patient/PatientCluster.php
@@ -5,10 +5,17 @@ namespace Modules\Patient\Filament\Clusters\Patient;
 use BackedEnum;
 use Filament\Clusters\Cluster;
 use Filament\Support\Icons\Heroicon;
+use Modules\Core\Enums\SidebarGroup;
 
 class PatientCluster extends Cluster
 {
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedSquares2x2;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedUsers;
+
+    protected static string|\UnitEnum|null $navigationGroup = SidebarGroup::PatientCare;
+
+    protected static ?int $navigationSort = 10;
+
+    protected static ?string $navigationLabel = 'Patients';
 
     protected static bool $shouldRegisterSubNavigation = false;
 }
